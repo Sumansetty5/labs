316bc33a12e07e009090051574225b6ee64198d2
Sumansetty5 <101685003+Sumansetty5@users.noreply.github.com>
2022-04-06 14:20:24 +0530

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -105,9 +105,9 @@ update_conf()
 
 ############### MAIN ###################
 
-USER="devops"
-GROUP="devops"
-passw="today@1234"
+USER="devop"
+GROUP="devop"
+passw="today@123"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
